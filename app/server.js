707036b7779dd9c05520fa413d97fb1944acb554
Kinark <igormarcossi@gmail.com>
2019-05-12 01:32:24 -0300

    Prettier

--- app/server.js
+++ app/server.js
@@ -3,8 +3,8 @@ require('./db')
 
 const app = require('express')()
 const cors = require('cors')
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const port = 3000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -17,17 +17,17 @@ app.use(cors)
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use((req, res, next) => {
-   res.type('json');
+   res.type('json')
    req.eita = 'hehehe'
-   next();
-});
-app.use(function (error, req, res, next) {
+   next()
+})
+app.use(function(error, req, res, next) {
    if (error instanceof SyntaxError) {
-      res.status(400).send({ msg: 'Something went really really wrong with the syntax (probably broken JSON).' });
+      res.status(400).send({ msg: 'Something went really really wrong with the syntax (probably broken JSON).' })
    } else {
-      next();
+      next()
    }
-});
+})
 app.use(require('./api/user/middlewares').reqWithJwt)
 
 //
