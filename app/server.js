5da75c3526d96bf9c9bb0eda382405dd710ab4b0
Kinark <igormarcossi@gmail.com>
2019-05-17 22:50:54 -0300

    Reorganized filed

--- app/server.js
+++ app/server.js
@@ -2,18 +2,21 @@ require('dotenv').config()
 require('./db')
 
 const app = require('express')()
-const cors = require('cors')
+
 const server = require('http').Server(app)
 // const io = require('socket.io')(server)
-const port = 3000
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
 
 //
 // ─── MIDDLEWARES ────────────────────────────────────────────────────────────────
 //
 
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const bearerToken = require('express-bearer-token')
+
 app.use(cors())
+app.use(bearerToken())
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use((req, res, next) => {
@@ -37,6 +40,8 @@ app.use(require('./api/user/middlewares').reqWithJwt)
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(require('./api/user/routes'))
+app.use(require('./api/world/routes'))
+// app.use(require('./api/message/routes'))
 
 app.all('*', (req, res) => res.status(404).send({ msg: 'not found' }))
 
@@ -44,4 +49,5 @@ app.all('*', (req, res) => res.status(404).send({ msg: 'not found' }))
 // ─── INITIALIZATION ─────────────────────────────────────────────────────────────
 //
 
+const port = 3000
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
