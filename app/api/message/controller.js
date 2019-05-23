c15d8fd1559cfec7756221bd367ce76b1a7d1688
Kinark <igormarcossi@gmail.com>
2019-05-23 00:48:26 -0300

    Basic message API

---
+++ app/api/message/controller.js
@@ -0,0 +1,6 @@
+const services = require('./services')
+const responseError = require('~/app/responseError')
+
+module.exports = {
+   
+}
--- app/api/message/model.js
+++ app/api/message/model.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
+const Author = new mongoose.Schema({
+   username: { type: String, required: true, trim: true },
+   _id: { type: String, required: true, trim: true },
+   avatar: { type: String, required: true, trim: true },
+})
+
 const MessageSchema = new mongoose.Schema({
-   author: { type: String, required: true, trim: true },
+   author: Author,
    content: { type: String, required: true, trim: true },
-   world: { type: String, required: true, trim: true },
+   room: { type: String, required: true, trim: true },
    type: { type: String, required: true, trim: true },
-   creationDate: { type: Date, required: true },
+   subRoom: { type: String, required: true, trim: true },
+   timestamp: { type: Date, required: true },
+   nonce: { type: Date, required: true },
 })
 
 const Message = mongoose.model('Message', MessageSchema)
