11a51f3fe41375abb8527152ffec355284b2d037
Kinark <igormarcossi@gmail.com>
2019-06-29 19:45:31 -0300

    Added activeMembers and pre remove task to World

--- app/api/world/model.js
+++ app/api/world/model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const Message = require('../message/model')
 
 const WorldSchema = new mongoose.Schema({
    owner: { type: String, required: true },
@@ -9,6 +11,7 @@ const WorldSchema = new mongoose.Schema({
    description: { type: String, trim: true },
    ruleset: { type: ObjectId },
    members: { type: [ObjectId] },
+   activeMembers: { type: [ObjectId] },
    password: { type: String },
    createdAt: { type: Date, required: true },
    tags: { type: [String] }
@@ -16,12 +19,17 @@ const WorldSchema = new mongoose.Schema({
 
 WorldSchema.pre('save', function(next) {
    const world = this
-   if(!world.password) next()
+   if (!world.password) next()
    bcrypt.hash(world.password, 10, (err, hash) => {
       if (err) return next(err)
       world.password = hash
       next()
    })
+})
+
+WorldSchema.pre('remove', function(next) {
+   Message.deleteMany({ room: this._id }).exec()
+   next()
 })
 
 const World = mongoose.model('World', WorldSchema)
