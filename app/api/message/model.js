25b84815926e45982ca93e87e1bc8d13c136e9d4
Kinark <igormarcossi@gmail.com>
2019-06-29 19:26:12 -0300

    String to ObjectId

--- app/api/message/model.js
+++ app/api/message/model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 const Author = new mongoose.Schema({
    username: { type: String, required: true, trim: true },
-   _id: { type: String, required: true, trim: true },
+   _id: { type: ObjectId, required: true, trim: true },
    avatar: { type: String, trim: true },
 })
 
 const MessageSchema = new mongoose.Schema({
    author: Author,
    content: { type: String, required: true, trim: true },
-   room: { type: String, required: true, trim: true },
+   room: { type: ObjectId, required: true, trim: true },
    type: { type: String, required: true, enum: ['adventure', 'talk'] },
    subRoom: { type: String, trim: true },
    timestamp: { type: Date, required: true },
