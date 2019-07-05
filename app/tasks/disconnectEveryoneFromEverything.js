5ae065053563f3cc96b1a26c31ef12b29e697a31
Kinark <igormarcossi@gmail.com>
2019-07-05 17:55:48 -0300

    Console.log added to function

--- app/tasks/disconnectEveryoneFromEverything.js
+++ app/tasks/disconnectEveryoneFromEverything.js
@@ -4,6 +4,7 @@ const updateWorlds = require('~/app/api/world/services').modifyWorlds
 const disconnectEveryoneFromEverything = async () => {
    await updateUsers({ currentSocket: { $ne: null } }, { currentSocket: null })
    await updateWorlds({ activeMembers: { $ne: [] } }, { activeMembers: [] })
+   console.log('Everyone was disconnected from everything')
 }
 
 module.exports = disconnectEveryoneFromEverything
